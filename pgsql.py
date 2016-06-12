9659e218b9da938c0181b7ba82646233a7e5055b
Ubuntu <bbdd@BaseDeDatos.xr4cgste4x1ejeovzueoynlrsf.nx.internal.cloudapp.net>
2016-06-12 16:26:56 +0000

    return float instead of Decimal

--- pgsql.py
+++ pgsql.py
@@ -3,6 +3,13 @@ import psycopg2
 import psycopg2.extras
 import screen
 
+
+DEC2FLOAT = psycopg2.extensions.new_type(
+    psycopg2.extensions.DECIMAL.values,
+    'DEC2FLOAT',
+    lambda value, curs: float(value) if value is not None else None)
+psycopg2.extensions.register_type(DEC2FLOAT)
+
 def get_pg_connection():
     """	Returns the pg_connection descriptor.
     To run this function, please first be sure to configure the tunnel
