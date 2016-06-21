1b0a9055a0dcd756d8cd1cd431df3712a933c7c6
lbarrios <m02@lbarrios.com.ar>
2016-06-21 01:33:42 -0300

    Reordenado pgsql-to-mongo.py

--- pgsql-to-mongo.py
+++ pgsql-to-mongo.py
@@ -3,30 +3,24 @@
 import screen
 import pgsql
 from converter import operacion_record_to_dict, factura_record_to_dict
-
+from pymongo import MongoClient
 
 screen.clearScreen()
+client = MongoClient()
+db = client.prod
 
 # Facturas
 print "Facturas:"
 facturas_cur = pgsql.get_DictCursor("facturas.sql")
 facturas = map(factura_record_to_dict, facturas_cur.fetchall())
 screen.prettyPrintJson2(facturas)
+db.facturas.delete_many({})
+db.facturas.insert_many(facturas)
 
 # Operaciones
 print "Operaciones:"
 operaciones_cur = pgsql.get_DictCursor("operaciones.sql")
 operaciones = map(operacion_record_to_dict, operaciones_cur.fetchall())
 screen.prettyPrintJson2(operaciones)
-
-
-from pymongo import MongoClient
-
-client = MongoClient()
-db = client.prod
-
-db.facturas.delete_many({})
-db.facturas.insert_many(facturas)
-
 db.operaciones.delete_many({})
 db.operaciones.insert_many(operaciones)
