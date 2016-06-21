802d83d646c5beef7267b46f7567de8e834fc453
lbarrios <m02@lbarrios.com.ar>
2016-06-21 01:33:12 -0300

    Borrados comentarios viejos

--- facturas.sql
+++ facturas.sql
@@ -1,37 +1,3 @@
-/*
---
--- Intento 1
---
-SELECT
-   usuario.id_usuario, month_index
-FROM
-   usuario CROSS JOIN generate_series(1, 12) as month_index;
-*/
-
-/*
---
--- Intento 2
---
-SELECT
-	usuario.id_usuario,
-	sum(comision) as monto_comisiones,
-	count(suscripcion)*37 as monto_suscripciones,
-	extract('month' from operacion.fecha)::int as mes
-FROM
-	operacion
-INNER JOIN publicacion ON publicacion.id_publicacion=operacion.id_publicacion
-LEFT JOIN usuario on usuario.id_usuario=publicacion.publicada_por
-FULL JOIN suscripcion ON usuario.id_usuario=suscripcion.id_usuario
-	AND extract('month' from operacion.fecha) = extract('month' from suscripcion.fecha_suscripcion)
-
-GROUP BY 
-	usuario.id_usuario,
-	extract('month' from operacion.fecha)
-*/
-
---
--- Intento 3
---
 SELECT
     usuario.id_usuario,
     mes,
